910afd8afde071c0fd40805e3134f2547f3e3a39
thepicture <fuzzyeastern@protonmail.com>
2021-11-25 16:41:03 +0000

    Added navigation to the modal control for qr code entering

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace QrCodeValidatorApp.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Func<object, bool> canExecute,
+                            Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute?.Invoke(parameter);
+        }
+    }
+}
--- ViewsModels/QrCodeViewModel.cs
+++ ViewsModels/QrCodeViewModel.cs
@@ -1,13 +1,36 @@
-﻿using QrCodeValidatorApp.Models;
+﻿using QrCodeValidatorApp.Commands;
+using QrCodeValidatorApp.Models;
+using System;
+using System.Windows.Input;
 
 namespace QrCodeValidatorApp.ViewsModels
 {
     class QrCodeViewModel : ViewModelBase
     {
         private QrCode _currentQrCode;
+        private bool _isQrCodeWriteMode = false;
+        private string _qrCodeText;
         public QrCodeViewModel()
         {
             Title = "Госуслуги";
+            SetQrCodeInputAsKeyboard = new RelayCommand(null,
+                                                       SetQrCodeMode);
+            CheckWrittenQrCode = new RelayCommand(IsWrittenQrCodeValid, ShowInvalidAttemptError);
+        }
+
+        private void ShowInvalidAttemptError(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsWrittenQrCodeValid(object arg)
+        {
+            return !string.IsNullOrWhiteSpace(QrCodeText);
+        }
+
+        private void SetQrCodeMode(object parameter)
+        {
+            IsQrCodeInWriteMode = Convert.ToBoolean(parameter);
         }
 
         public QrCode CurrentQrCode
@@ -15,6 +38,27 @@ namespace QrCodeValidatorApp.ViewsModels
             get => _currentQrCode; set
             {
                 _currentQrCode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SetQrCodeInputAsKeyboard { get; set; }
+        public ICommand CheckWrittenQrCode { get; set; }
+
+        public bool IsQrCodeInWriteMode
+        {
+            get => _isQrCodeWriteMode; set
+            {
+                _isQrCodeWriteMode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string QrCodeText
+        {
+            get => _qrCodeText; set
+            {
+                _qrCodeText = value;
                 OnPropertyChanged();
             }
         }
